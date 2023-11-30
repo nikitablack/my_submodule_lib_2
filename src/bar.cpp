188bfd5c212d700c18dba583bec5e04c9e036749
nc <nikita@nodarsensor.com>
2023-11-30 09:15:06 +0100

    Update submodule submodules/my_submodule_lib_2 to latest master

--- src/bar.cpp
+++ src/bar.cpp
@@ -4,11 +4,7 @@
 
 void bar()
 {
-<<<<<<< HEAD
-    constexpr int32_t LIB_VERSION{4};
-=======
     constexpr int32_t LIB_VERSION{3};
->>>>>>> bfd83bea267caeffefa362e06a713c60caf7b3dd
 
     std::cout << "My Submodule Lib 2: " << LIB_VERSION << std::endl;
 
